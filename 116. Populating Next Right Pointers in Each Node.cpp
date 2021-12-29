64bd93f7f39c94d59d8c360fd85cbf987e6afc97
Sakshi Agarwal <67694526+7sakshi7@users.noreply.github.com>
2021-12-29 21:23:11 +0530

    Update 116. Populating Next Right Pointers in Each Node.cpp

--- 116. Populating Next Right Pointers in Each Node.cpp
+++ 116. Populating Next Right Pointers in Each Node.cpp
@@ -53,3 +53,54 @@ public:
         return root;
     }
 };
+
+// 2nd Approach Using one queue with 88 %faster time
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(root == NULL)return NULL;
+        
+        queue<pair<int,Node*>>q,nextPointers;
+        q.push({0,root});
+        nextPointers.push({0,root});
+        
+        while(!q.empty()){
+            pair<int,Node*>p = q.front();
+            q.pop();
+            if(p.second->left){
+                q.push({p.first+1,p.second->left});
+                nextPointers.push({p.first+1,p.second->left});
+            }
+            
+            if(p.second->right){
+                q.push({p.first+1,p.second->right});
+                nextPointers.push({p.first+1,p.second->right});
+            }
+             if(q.empty() || p.first!=q.front().first){
+                p.second->next = NULL;
+            }
+            else{
+                p.second->next = q.front().second;
+            }
+        }
+        return root;
+    }
+};
