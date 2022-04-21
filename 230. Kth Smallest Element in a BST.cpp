b8ef1720bbe62a1818ebf80a6f5df6f78a00bc85
Sakshi Agarwal <67694526+7sakshi7@users.noreply.github.com>
2022-04-21 18:51:06 +0530

    Create 230. Kth Smallest Element in a BST.cpp

---
+++ 230. Kth Smallest Element in a BST.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void KElements(TreeNode* root,int k,vector<int>&v){
+        if(root == NULL)
+            return;
+        
+        KElements(root->left,k,v);
+        v.push_back(root->val);
+        KElements(root->right,k,v);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int>v;
+        KElements(root,k,v);
+        return v[k-1];
+    }
+};
